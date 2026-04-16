7ec2793dff7c17ca67d19cb86cef27f1329c7e38
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2026-04-16 23:42:18 +0200

    Ajout du pipeline CI pour les tests Jest

--- backend/src/Api/middleware/rateLimitingMiddleware.ts
+++ backend/src/Api/middleware/rateLimitingMiddleware.ts
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 export const loginLimiter = rateLimit({
     // 15 minutes
   windowMs: 15 * 60 * 1000, 
-  // Limite chaque IP à 5 tentatives de connexion par 15 min
+  // Limite à 5 tentatives de connexion par 15 min
   max: 5, 
   message: {
     success: false,
